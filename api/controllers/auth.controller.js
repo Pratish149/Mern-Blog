f0658a0bbee22ba9a8dac41c08829f62a8b221d9
Pratish Srivastava <pratishsrivastava149.ps@gmail.com>
2024-05-14 23:56:33 +0530

    Created signup api and added middleware for errors

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+const User = require("../models/user.model.js");
+const { errorHandler } = require("../utils/error.js");
+const bcrypt = require("bcryptjs");
+
+const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.status(200).json({ message: "Signup successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  signup,
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const userRouter = require("./routes/user.route");
+const authRouter = require("./routes/auth.route");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -13,8 +15,24 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    failed: true,
+    statusCode,
+    message,
+  });
 });
