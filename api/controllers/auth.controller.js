1db8f0c348fac8662eba9f60d3c1b572a2642fce
Pratish Srivastava <pratishsrivastava149.ps@gmail.com>
2024-05-21 23:42:53 +0530

    Created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const User = require("../models/user.model.js");
 const { errorHandler } = require("../utils/error.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,6 +29,35 @@ const signup = async (req, res, next) => {
   }
 };
 
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json({ status: "SUCCESS", message: "Signin successful", token });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   signup,
+  signin,
 };
