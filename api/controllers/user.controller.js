ef53a1452a1b468d02fdf3163869e76e9a45134d
Pratish Srivastava <pratishsrivastava149.ps@gmail.com>
2024-05-31 00:00:03 +0530

    Fixed update user api functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,30 +9,36 @@ const test = (req, res) => {
 };
 
 const updateUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  const { user, params, body } = req || {};
+  const { id } = user || {};
+  const { userId } = params || {};
+  const { username, password, email, profilePicture } = body || {};
+  let updatedPassword = undefined;
+
+  if (id !== userId) {
     return next(errorHandler(403, "You are not allowed to update this user"));
   }
-  if (req.body.password) {
-    if (req.body.password.length < 6) {
+  if (password !== undefined) {
+    if (password.length < 6) {
       return next(
         errorHandler(400, "Password must be at least 6 characters long")
       );
     }
-    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    updatedPassword = bcrypt.hashSync(password, 10);
   }
-  if (req.body.username) {
-    if (req.body.username.length < 7 || req.body.username.length > 20) {
+  if (username !== undefined) {
+    if (username.length < 7 || username.length > 20) {
       return next(
         errorHandler(400, "Username must be between 7 and 20 characters")
       );
     }
-    if (req.body.username.includes(" ")) {
+    if (username.includes(" ")) {
       return next(errorHandler(400, "Username cannot contain spaces"));
     }
-    if (req.body.username !== req.body.username.toLowerCase()) {
+    if (username !== username.toLowerCase()) {
       return next(errorHandler(400, "Username must be lowercase"));
     }
-    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+    if (!username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
         errorHandler(400, "Username can only contain letters and numbers")
       );
@@ -43,10 +49,10 @@ const updateUser = async (req, res, next) => {
       req.params.userId,
       {
         $set: {
-          username: req.body.username,
-          password: req.body.password,
-          email: req.body.email,
-          profilePicture: req.body.profilePicture,
+          username,
+          password: updatedPassword,
+          email,
+          profilePicture,
         },
       },
       { new: true }
